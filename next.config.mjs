4c6f9f0f8a413c78b3b9d3ec683da1b5327330cf
Rohan Phulkar <rohanphulkar936@gmail.com>
2025-10-18 14:24:22 +0530

    chore: github action removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        "@clerk/clerk-react": "@clerk/nextjs",
-      },
-    },
-  },
-  env: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  },
 };
 
 export default nextConfig;

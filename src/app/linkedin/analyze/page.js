dda31e53691b7a092b7ba6f8b2ff9a294e5ee380
Rohan Phulkar <rohanphulkar936@gmail.com>
2025-11-15 17:00:47 +0530

    fix: loading screen hide

--- src/app/linkedin/analyze/page.js
+++ src/app/linkedin/analyze/page.js
@@ -44,7 +44,7 @@ const LinkedinAnalyze = () => {
   });
   const [isLoadingPricing, setIsLoadingPricing] = useState(true);
 
-  const [isAnalyzing, setIsAnalyzing] = useState(true);
+  const [isAnalyzing, setIsAnalyzing] = useState(false);
   const [processData, setProcessData] = useState({
     title: "Starting Analysis",
     subtitle: "Initializing LinkedIn profile analysis",
